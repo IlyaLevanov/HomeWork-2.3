056f1c79a65a18fd3b38856d302cc6adc7f8ac9a
Ilya Levanov <ilya.levanov@inbox.ru>
2021-09-25 18:01:49 +0300

    forgot UN and PWD buttons logic added

--- HomeWork-2.3/LoginViewController.swift
+++ HomeWork-2.3/LoginViewController.swift
@@ -9,12 +9,42 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet var userNameTextField: UITextField!
+    @IBOutlet var passwordTextFiel: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
 
     }
     
+    @IBAction func logInButtonPress() {
+        
+    }
+    @IBAction func forgotUNButtonPress() {
+        forgotSmthAlert(title: "Oops!", message: "Your user name is: 'User'")
+    }
+    @IBAction func forgotPSWDButtonPress() {
+        forgotSmthAlert(title: "Oops!", message: "Your password is: 'Password'")
+    }
+    
 
+}
 
+// MARK: - AlertViewControllers
+extension LoginViewController{
+    private func forgotSmthAlert(title: String, message: String){
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+    
+    private func wrongDataAlert(title: String, message: String){
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "Try again", style: .default)
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
 }
